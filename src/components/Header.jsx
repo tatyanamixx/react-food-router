69953fb8d9e3a59c1ca189c3abb87d3fc0c8bb43
tatyanaMixx <tatyanamixx@gmal.com>
2024-11-25 10:23:05 +0500

    add menu for mobile to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,22 @@ import { Link } from 'react-router-dom';
 
 function Header() {
 	return (
-		<nav className='green darken-1'>
+		<nav className='green darken-1 nav-extended'>
 			<div className='container'>
 				<div className='nav-wrapper'>
-					<Link to='/' className='brand-logo'>
-						React Food
+					<Link to='/' className='brand-logo center'>
+						RECIPE
 					</Link>
-
-					<ul id='nav-mobile' className='right hide-on-med-and-down'>
-						<li>
+				</div>
+				<div className='nav-content'>
+					<ul className='tabs tabs-transparent'>
+						<li className='tab'>
 							<Link to={`/catalog`}>Catalog</Link>
 						</li>
-						<li>
+						<li className='tab'>
 							<Link to={`/about`}>About</Link>
 						</li>
-						<li>
+						<li className='tab'>
 							<Link to={`/contacts`}>Contacts</Link>
 						</li>
 					</ul>

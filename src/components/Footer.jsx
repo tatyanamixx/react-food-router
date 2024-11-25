decbd4b1e530dba3e38a8fc66dc5cefd6309c7af
tatyanamixx <66796387+tatyanamixx@users.noreply.github.com>
2024-11-25 10:45:26 +0500

    Update Footer.jsx
    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
                     © {new Date().getFullYear()} Copyright Text
                     <a
                         className='grey-text text-lighten-4 right'
-                        href='https://github.com/michey85/react-food'
+                        href='https://github.com/tatyanamixx/react-food-router'
                         rel='noreferrer'
                         target='_blank'
                     >

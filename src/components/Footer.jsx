75ea8325114119ca1ebda9d0821b036c35ebf09b
tatyanaMixx <tatyanamixx@gmal.com>
2024-11-25 10:36:36 +0500

    change link to repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,20 @@
 function Footer() {
-    return (
-        <footer className='page-footer green lighten-4'>
-            <div className='footer-copyright'>
-                <div className='container'>
-                    © {new Date().getFullYear()} Copyright Text
-                    <a
-                        className='grey-text text-lighten-4 right'
-                        href='https://github.com/michey85/react-food'
-                        rel='noreferrer'
-                        target='_blank'
-                    >
-                        Repo
-                    </a>
-                </div>
-            </div>
-        </footer>
-    );
+	return (
+		<footer className='page-footer green lighten-4'>
+			<div className='footer-copyright'>
+				<div className='container'>
+					© {new Date().getFullYear()} Copyright
+					<a
+						className='grey-text text-lighten-4 right'
+						href='https://github.com/tatyanamixx/react-food-router.git'
+						rel='noreferrer'
+						target='_blank'>
+						Repo
+					</a>
+				</div>
+			</div>
+		</footer>
+	);
 }
 
 export { Footer };
